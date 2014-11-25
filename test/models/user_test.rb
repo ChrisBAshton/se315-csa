f54a1d9a06bdae5b825bf5fcee9cd7521e342f1a
ChrisBAshton <chrisashtonweb@gmail.com>
2014-11-25 11:35:18 +0000

    expanded user unit tests according to worksheet 4

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,9 +14,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "grad_year date range between 1970 and this year" do
-    user = User.new(:firstname => "Chris",
-                    :surname => "Loftus",
-                    :email => "cwl1@aber.ac.uk")
+    user = User.new(:firstname => users(:one).firstname,
+                    :surname =>   users(:one).surname,
+                    :email =>     "string_to_make_email_unique" + users(:one).email)
     # Do boundary checks
     user.grad_year = 1969
     assert !user.valid?
@@ -37,10 +37,31 @@ class UserTest < ActiveSupport::TestCase
   # Try writing some tests to check that the email is formatted correctly
 
   test "unique email" do
-    user = User.new(firstname: "Chris",
-                    surname: "Loftus",
-                    grad_year: 1985,
-                    email: users(:one).email)
+    user = User.new(firstname: users(:one).firstname,
+                    surname:   users(:one).surname,
+                    grad_year: users(:one).grad_year,
+                    email:     users(:one).email)
+    assert !user.valid?
+  end
+
+  test "valid email" do
+    user = User.new(firstname: users(:one).firstname,
+                    surname:   users(:one).surname,
+                    grad_year: users(:one).grad_year)
+    
+    user.email = 'cba1@aber.ac.uk'
+    assert user.valid?
+
+    user.email = 'dara_o-bria3inX@convoluted-emaildomain.co.nz'
+    assert user.valid?
+
+    user.email = ''
+    assert !user.valid?
+
+    user.email = 'bob@'
+    assert !user.valid?
+
+    user.email = 'bob@bob@bob.com'
     assert !user.valid?
   end
 end
