9bf8ce25383048015bdf1b4a0411dba32daf12ce
ChrisBAshton <chrisashtonweb@gmail.com>
2014-11-26 13:49:29 +0000

    tried setting rails env

--- config/code_cov.rb
+++ config/code_cov.rb
@@ -1,9 +1,9 @@
 require 'simplecov'
 SimpleCov.minimum_coverage MIN_CODE_COVERAGE
 SimpleCov.start do
-  # add_filter 'features/'
-  # add_filter 'config/'
-  # add_filter 'helpers/'
+  add_filter 'features/'
+  add_filter 'config/'
+  add_filter 'helpers/'
 end
 
 ENV['CODECOV_TOKEN']='4d2c59d8-d7a0-4f30-aae0-4357dac3b199'
