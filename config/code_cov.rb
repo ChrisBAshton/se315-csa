bf45198004b4b7226f85a1a99eda46d703c96a11
ChrisBAshton <chrisashtonweb@gmail.com>
2014-11-26 14:08:11 +0000

    another try

--- config/code_cov.rb
+++ config/code_cov.rb
@@ -8,7 +8,7 @@ end
 
 ENV['CODECOV_TOKEN']='4d2c59d8-d7a0-4f30-aae0-4357dac3b199'
 
-# if ENV['CI']=='true'
-#   require 'codecov'
-#   SimpleCov.formatter = SimpleCov::Formatter::Codecov
-# end
+require 'codecov'
+if ENV['CI'] == 'true'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
