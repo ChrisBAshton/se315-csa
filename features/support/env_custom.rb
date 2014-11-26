e66007a54bcf2698e27abec3a0fc89762fa363b9
ChrisBAshton <chrisashtonweb@gmail.com>
2014-11-26 17:50:20 +0000

    clearer code

--- features/support/env_custom.rb
+++ features/support/env_custom.rb
@@ -1,23 +1,22 @@
 # MONITOR CODE COVERAGE
 require 'simplecov'
 
-# CLEAN DATABASE IN BETWEEN SCENARIOS
-require 'database_cleaner'
+# SUPPORT JAVASCRIPT FOR SPECIFIC CUCUMBER SCENARIOS
+# I use Poltergeist because it is headless and a little quicker than selenium. It's also pre-installed on Travis by default.
+# The JavaScript driver is, naturally, much slower than a non JavaScript driver, so I've only used the `@javascript` tag where necessary
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist #default driver when you using @javascript tag
 
+# CLEAN DATABASE IN BETWEEN SCENARIOS
 # Credit mostly to Helen Harman:
 # https://www.facebook.com/Se31520/posts/689637187799632?comment_id=689646561132028&offset=0&total_comments=1
+require 'database_cleaner'
 Before do
   DatabaseCleaner.strategy = :truncation
   DatabaseCleaner.start
   DatabaseCleaner.clean
   load Rails.root.join('db/seeds.rb')
 end
-
 After do |scenario|
   DatabaseCleaner.clean
 end
-
-# Set a javascript driver to allow JS to be executed
-# I use Poltergeist because it is headless and a little quicker than selenium. It's also pre-installed on Travis by default.
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist #default driver when you using @javascript tag
