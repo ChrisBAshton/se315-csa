32f4f85a65ffdc0ec411718725ef13ab38f509c0
ChrisBAshton <chrisashtonweb@gmail.com>
2014-11-26 13:33:25 +0000

    tweaks for travis

--- config/code_cov.rb
+++ config/code_cov.rb
@@ -1,14 +1,14 @@
 require 'simplecov'
 SimpleCov.minimum_coverage MIN_CODE_COVERAGE
 SimpleCov.start do
-  add_filter 'features/'
-  add_filter 'config/'
-  add_filter 'helpers/'
+  # add_filter 'features/'
+  # add_filter 'config/'
+  # add_filter 'helpers/'
 end
 
-CODECOV_TOKEN='4d2c59d8-d7a0-4f30-aae0-4357dac3b199'
+ENV['CODECOV_TOKEN']='4d2c59d8-d7a0-4f30-aae0-4357dac3b199'
 
-if ENV['CI']=='true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
+# if ENV['CI']=='true'
+#   require 'codecov'
+#   SimpleCov.formatter = SimpleCov::Formatter::Codecov
+# end
